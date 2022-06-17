ea5ed89d9456e83b9f3b3a5facc640d236c43327
Leah E. Lackner <leah.lackner+github@gmail.com>
2022-06-17 07:38:50 +0200

    Initial commit

---
+++ tests/test_template_python_project_main.py
@@ -0,0 +1,9 @@
+from template_python_project import api
+
+
+def test_template_python_project_api_some() -> None:
+    api.some_api_func()
+
+
+def test_template_python_project_api_other() -> None:
+    api.other_api_func()
