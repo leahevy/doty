9d365cb5cd56806d6031f397c764e510d1062a55
Leah Lackner <leah.lackner+github@gmail.com>
2022-06-17 08:54:08 +0200

    Inject package with fixture + license headers

--- tests/test_doty_main.py
+++ tests/test_doty_main.py
@@ -1,9 +1,26 @@
-from doty import api
+#!/usr/bin/env python
+#
+# Copyright (C) 2022 Leah Lackner
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from types import ModuleType
 
 
-def test_doty_api_some() -> None:
-    api.some_api_func()
+def test_doty_api_some(doty: ModuleType) -> None:
+    doty.api.some_api_func()
 
 
-def test_doty_api_other() -> None:
-    api.other_api_func()
+def test_doty_api_other(doty: ModuleType) -> None:
+    doty.api.other_api_func()
